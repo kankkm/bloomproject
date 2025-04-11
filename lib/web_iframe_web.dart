0fce2bd87eff3d5275cd5798e1994e8b10461191
kankkm <71086038+kankkm@users.noreply.github.com>
2025-04-11 21:36:58 +0700

    error fix1
    error fix1

--- lib/web_iframe_web.dart
+++ lib/web_iframe_web.dart
@@ -1,19 +1,33 @@
-// web_iframe_web.dart
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
 import 'dart:ui' as ui;
-import 'dart:html';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 
-Widget buildWebView(String viewId, String url) {
-  // ignore: undefined_prefixed_name
-  ui.platformViewRegistry.registerViewFactory(viewId, (int viewId) {
-    final iframe = IFrameElement()
-      ..width = '100%'
-      ..height = '100%'
-      ..src = url
-      ..style.border = 'none'
-      ..allow = 'autoplay; fullscreen';
-    return iframe;
-  });
+class WebIframe extends StatelessWidget {
+  final String viewId;
+  final String url;
 
-  return HtmlElementView(viewType: viewId);
+  const WebIframe({super.key, required this.viewId, required this.url});
+
+  @override
+  Widget build(BuildContext context) {
+    if (kIsWeb) {
+      // ✅ ใช้ registerViewFactory แบบนี้
+      // ignore: undefined_prefixed_name
+      ui.platformViewRegistry.registerViewFactory(viewId, (int viewId) {
+        final iframe = html.IFrameElement()
+          ..width = '100%'
+          ..height = '100%'
+          ..src = url
+          ..style.border = 'none'
+          ..allow = 'autoplay; fullscreen';
+        return iframe;
+      });
+
+      return HtmlElementView(viewType: viewId);
+    } else {
+      return const Text('Only available on web');
+    }
+  }
 }
